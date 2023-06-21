1d595aafe84b9ce21fec8d92b14ccaa59be0a121
vishalbhoyar24 <124866977+vishalbhoyar24@users.noreply.github.com>
2023-06-21 11:46:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+let element = document.querySelector("#level");
+let level = 0;
+while (element !== null) {
+  console.log(element);
+  element = element.parentNode;
+  level++;
+}
+alert("The level of the element is: " + level);
